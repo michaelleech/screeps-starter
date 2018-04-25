0b139017572fba9a2e228943e9c0105feafde379
Dylan Larrabee <dylan.larrabee@makewonder.com>
2018-04-25 00:46:44 -0700

    Initial commit and setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Screeps starter!
+
+navigate to "~/Library/Application Support/Screeps/scripts/{server_location}"
+
+`git clone (this repo)`
+
+rename this folder to "default"
+
+`npm install`
+
+`npm run build`
+
+Open up main.ts and get crackin'
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  devtool: "source-map",
+  watch: true,
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname)
+  }
+};
